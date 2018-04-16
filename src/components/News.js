f47d3d031f2d9d05ebec69c7786aa04621630cf7
Ben Atkins <benjatkins13@gmail.com>
2018-04-16 15:14:15 -0500

    setting up news module

--- src/components/News.js
+++ src/components/News.js
@@ -6,45 +6,45 @@ import React from 'react';
 
 
 
-// function to format News as desired
+// Format News as desired
 
 
-// class NewsList extends React.Component{
-//     render(){
-//       var styles = {
+class NewsList extends React.Component{
+    render(){
+      var styles = {
           
-//         // Example format for styling:
+        // Example format for styling:
 
 
-//         // listGroup: {
-//         //   margin: '5px 0'
-//         // }
+        listGroup: {
+          margin: '5px 0'
+        }
 
-//       };
-//       var listItems = this.props.items.map((item, index) => {
-//         return (
+      };
+      var listItems = this.props.items.map((item, index) => {
+        return (
 
-//             example:
+            // example:
 
-//           <li key={index} className="list-group-item" style={styles.listGroup}>
+          <li key={index} className="list-group-item" style={styles.listGroup}>
 
-//           </li>
-//         )
-//       });
-//       return (
-
-
-//         example:
-
-//         <div className="col-sm-12">
-//           <ul className="list-group">
-//             {listItems}
-//           </ul>
-//         </div>
+          </li>
+        )
+      });
+      return (
 
 
-//       )
-//     }
-//   };
+        // example:
+
+        <div className="col-sm-12">
+          <ul className="list-group">
+            {listItems}
+          </ul>
+        </div>
+
+
+      )
+    }
+  };
   
-//   export default NewsList;
+  export default NewsList;
