e6c545ef727bba6cc167cc3de840adcf503efc34
Ben Atkins <benjatkins13@gmail.com>
2018-04-16 14:38:47 -0500

    comments for planning

--- src/components/News.js
+++ src/components/News.js
@@ -7,3 +7,44 @@ import React from 'react';
 
 
 // function to format News as desired
+
+
+// class NewsList extends React.Component{
+//     render(){
+//       var styles = {
+          
+//         // Example format for styling:
+
+
+//         // listGroup: {
+//         //   margin: '5px 0'
+//         // }
+
+//       };
+//       var listItems = this.props.items.map((item, index) => {
+//         return (
+
+//             example:
+
+//           <li key={index} className="list-group-item" style={styles.listGroup}>
+
+//           </li>
+//         )
+//       });
+//       return (
+
+
+//         example:
+
+//         <div className="col-sm-12">
+//           <ul className="list-group">
+//             {listItems}
+//           </ul>
+//         </div>
+
+
+//       )
+//     }
+//   };
+  
+//   export default NewsList;
