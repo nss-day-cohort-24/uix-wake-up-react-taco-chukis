42470e195002ba99bf0ef3ac0db835dc31f2019f
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-04-16 15:13:30 -0500

    base tiles there.

--- uix-wake-up-react-taco-chukis/src/App.js
+++ uix-wake-up-react-taco-chukis/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import holderImg from './img/museum.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="">
+        <header>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div className="d-flex flex-row justify-content-around">
+        {/* weather hockey */}
+
+        {/* weather */}
+          <div className="card-tile my-3 mx-2 p-3 col-md-6">
+            <p className="display-3">33</p>
+          </div>
+        {/* end hockey */}
+          
+        {/* hockey */}
+          <div className="card-tile my-3 mx-2 p-3 col-md-6">
+            <p className="">Hockey Stats</p>          
+          </div>
+        {/* end hockey */}
+        </div>
+        {/* end weather/hockey */}
+        {/* news */}
+        <div>
+        {/* news card */}
+          <div className="card-tile card my-3 mx-2">
+            <img className="card-img-top" src={holderImg} alt="Card image cap" />
+              <div className="card-tile card-body">
+                <p className="card-tile card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+        {/* end news card  */}
+        </div>
+      {/* end news */}
       </div>
     );
   }
