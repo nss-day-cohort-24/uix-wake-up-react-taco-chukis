dbf36f0b01a02f3307f675f1d38ccd1942c48eb1
Meg Scholl <meghan.e.scholl@gmail.com>
2018-04-17 11:47:22 -0500

    setting up to render the news articles inside the app in separate cards

--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
-// import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import holderImg from '../img/museum.jpg';
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
 
-// API Call(fetch) for weather using zipcode
-// 
 
-// function to format News as desired
+function NewsSetup(props) {
+    return (
 
+        <div>
+          <div className="card-tile card my-3 mx-2">
+            <img className="card-img-top" src={holderImg} alt="Card image cap" />
+              <div className="card-tile card-body">
+                <p className="card-tile card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              
+              </div>
+          </div>
+
+
+      <Card>
+        {props.newsLoaded ?
+           <span>
+              <CardTitle>{props.newsTitle}</CardTitle>
+
+              <CardText>{props.newsTitle}</CardText>
+           </span>
+           :
+           <CardTitle>US News</CardTitle>
+        }
+        {props.showResult ?
+            <div> </div> 
+            :
+            <div>nope</div>
+        }
+      </Card>
+      </div>
+
+    )
+}
 
 //render top 10 news results in the US
 
@@ -43,9 +73,9 @@ class News extends Component {
                 (result) => {
                     this.setState({
                         newsLoaded: true,
-                        objResult: result
+                        objResult: result.articles
                     });
-                    console.log("result: ", result);
+                    console.log("result: ", result.articles);
                 },
                 (error) => {
                     this.setState({
@@ -70,7 +100,14 @@ class News extends Component {
                 return <div>Loading...</div>
             } else{
                 return (
-                    <div>News</div>
+                    <div>
+                        <NewsSetup newsLoaded={newsLoaded}
+                        newsTitle={objResult.title}
+                        newsContent={objResult.description}
+                        showResult={showResult}
+                        showClicked={this.showClicked}
+                        />
+                    </div>
                 )
             }
         }
