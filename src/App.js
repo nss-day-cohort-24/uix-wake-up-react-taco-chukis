0e24ee42558c19b43ae038befae38b62da405935
Meg Scholl <meghan.e.scholl@gmail.com>
2018-04-17 09:39:53 -0500

    setting up to render news. rendering 'loading' now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import News from './components/News';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <News />
       </div>
     );
   }
