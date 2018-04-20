17f1799e521088fdb4631518f3029fb16932082b
Ben Atkins <benjatkins13@gmail.com>
2018-04-20 13:31:43 -0500

    fix bugs

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
       }
     }
     this.updateZip = this.updateZip.bind(this);
+    this.updateTeam = this.updateTeam.bind(this);
   }
   
 componentDidMount() {
@@ -38,7 +39,7 @@ componentDidMount() {
               loading: false,
               userObj: {
                 zip: user.zip,
-                uid: user.uid
+                uid: user.uid,
               }
               
             });
@@ -81,6 +82,22 @@ updateZip(zipCode){
   })
 }
 
+updateTeam(team){
+  console.log("updateTeam", team);
+  const userObj = {...this.state.userObj};
+  userObj.team = team;
+  this.setState({userObj});
+  console.log("userobj", {userObj})
+
+  var userRef = firebase.database().ref(`/users/${this.state.userObj.uid}`);
+        userRef.update({ team: team });
+
+   this.ref = rebase.syncState(`/users/${this.state.userObj.uid}/`, {
+      context: this,
+      state: 'userObj'
+  })
+}
+
 
   render() {
     return (
@@ -111,7 +128,8 @@ updateZip(zipCode){
             
         {/* hockey */}
           <div className="card-tile my-3 mx-2 p-3 col-md-6">    
-            <HockeyMain uid={this.state.uid}/>    
+            <HockeyMain uid={this.state.userObj}
+            updateTeam={this.updateTeam}/>    
           </div>
         {/* end hockey */}
         </div>
